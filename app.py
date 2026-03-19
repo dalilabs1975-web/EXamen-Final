4ce667011442ad19fabd0bd82ead21a6d6e1be52
bensaadadalila6-boop <225903538+dalilabs1975-web@users.noreply.github.com>
2026-03-19 10:58:05 -0400

    Commit Initial

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/exercice')
+def exercice():
+    # Remplacez par votre nom et prénom
+    prenom = "Dalila"
+    nom = "Ben Saada"
+    return f"<h1>{prenom} {nom}</h1>"
+
+if __name__ == '__main__':
+    # L'application écoute sur le port 5000 par défaut
+    # Si le port est pris, vous pouvez changer le port ici
+    app.run(host='0.0.0.0', port=5000)
